08c9879522df228afc3e06c1f73c045ac070aef9
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-02 11:52:56 -0700

    add select all and select a column

--- practice_on_twitter.sql
+++ practice_on_twitter.sql
@@ -6,6 +6,10 @@ SHOW DATABASES;                                         -- show the databases in
 
 SHOW TABLES IN twitter;                                 -- show the tables in twitter database
 
+SELECT * FROM users;                                    -- shows all the data in the users table
+
+SELECT first_name FROM users;                           -- gets the first name from all the users
+
 -- UPDATE
 
 -- DELETE
