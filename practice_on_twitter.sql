627a7df16d909587378e1c7fb77f852079a02279
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-02 12:10:44 -0700

    add a select with conditional and sorting

--- practice_on_twitter.sql
+++ practice_on_twitter.sql
@@ -14,6 +14,13 @@ SELECT first_name, last_name FROM users WHERE id = 2 OR id = 3;
                                                         -- Select with Conditionals
                                                         -- This gets the first name and last name of users with id of 2 or 3
 
+SELECT * FROM users WHERE first_name LIKE "%e";         -- gets all the users where their first name ends in e
+
+SELECT * FROM users ORDER BY birthday DESC;             -- gets all the users sorted by birthday with the youngest user at the top
+
+SELECT * FROM users WHERE first_name LIKE "%e" ORDER BY birthday DESC;
+                                                        -- gets all the users where their first name ends in e sorted by birthday with the youngest user at the top
+
 -- UPDATE
 
 -- DELETE
