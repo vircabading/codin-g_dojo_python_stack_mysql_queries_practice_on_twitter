e07e1023de9484c3f7cd1420969fbb2585600be5
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-02 11:57:24 -0700

    add select with conditionals

--- practice_on_twitter.sql
+++ practice_on_twitter.sql
@@ -10,6 +10,10 @@ SELECT * FROM users;                                    -- shows all the data in
 
 SELECT first_name FROM users;                           -- gets the first name from all the users
 
+SELECT first_name, last_name FROM users WHERE id = 2 OR id = 3;
+                                                        -- Select with Conditionals
+                                                        -- This gets the first name and last name of users with id of 2 or 3
+
 -- UPDATE
 
 -- DELETE
