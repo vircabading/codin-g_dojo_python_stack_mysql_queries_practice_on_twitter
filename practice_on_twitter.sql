a3c9b78352bf6706fd71ed6daf41b0372a89b1df
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-02 11:50:52 -0700

    add show databases and show tables

---
+++ practice_on_twitter.sql
@@ -0,0 +1,11 @@
+-- CREATE
+
+-- RETRIEVE
+
+SHOW DATABASES;                                         -- show the databases in mySQL
+
+SHOW TABLES IN twitter;                                 -- show the tables in twitter database
+
+-- UPDATE
+
+-- DELETE
